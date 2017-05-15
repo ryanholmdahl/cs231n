c8e7f6b5e94bb15ee571f6f48f6b98044b017d89
Ryan Holmdahl <ryholmdahl@gmail.com>
2017-05-14 22:19:01 -0700

    Add show_image_example

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+from util import show_image_example
 from baseline_models import Conv2ConvModel
 
 params = {}
@@ -9,12 +10,12 @@ params['lr_decay_steps'] = 100
 params['im_width'] = 128
 params['im_height'] = 128
 params['im_channels'] = 3
-params['in_conv_layers'] = 1
+params['in_conv_layers'] = 0
 params['in_conv_filters'] = [1]
 params['in_conv_dim'] = [5]
 params['in_conv_stride'] = [2]
-params['fc_layers'] = 1
-params['fc_dim'] = [128]
+params['fc_layers'] = 2
+params['fc_dim'] = [1024, 1024]
 params['embed_channels'] = 100
 params['out_conv_layers'] = 2
 params['out_conv_filters'] = [8, 16]
@@ -25,7 +26,7 @@ params['output_conv_stride'] = 1
 params["model_name"] = "test"
 params["ckpt_path"] = "ckpt"
 params["log_path"] = "log"
-params["n_epochs"] = 100
+params["n_epochs"] = 10
 params["n_eval_batches"] = 10
 params["batch_size"] = 16
 
@@ -54,9 +55,11 @@ train_examples = [np.array([np.array(tup[0]) for tup in train_tuples]), np.array
 valid_tuples = [gen.next() for _ in range(100)]
 valid_examples = [np.array([np.array(tup[0]) for tup in valid_tuples]), np.array([np.array(tup[1]) for tup in valid_tuples])]
 
+
 m = Conv2ConvModel(params)
 m.build()
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     m.fit(sess, tf.train.Saver(), train_examples, valid_examples)
+    show_image_example(sess, m, train_tuples[0][0], train_tuples[0][1])
 

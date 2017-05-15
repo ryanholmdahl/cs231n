f38acc8066110e32fcff8856cd998707abf407b2
Ryan Holmdahl <ryholmdahl@gmail.com>
2017-05-14 23:23:44 -0700

    Add Affine2Affine

--- baseline_models.py
+++ baseline_models.py
@@ -61,3 +61,34 @@ class Conv2ConvModel(Model):
         if outputs_batch is not None:
             feed_dict[self.truth_in] = outputs_batch
         return feed_dict
+
+class Affine2AffineModel(Model):
+    def add_placeholders(self):
+        input_dims = (None, self.config.im_height, self.config.im_width, self.config.im_channels)
+        self.image_in = tf.placeholder(tf.float32, shape=input_dims)
+        self.truth_in = tf.placeholder(tf.float32, input_dims)
+        self.global_step = tf.Variable(0, trainable=False)
+
+    def add_prediction_op(self):
+        prev_output = self.image_in
+        prev_output = tf.contrib.layers.flatten(prev_output)
+        for i in range(self.config.fc_layers):
+            prev_output = tf.layers.dense(prev_output, self.config.fc_dim[i], activation=tf.nn.relu,
+                                          kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                          name='fc{}'.format(i))
+        prev_output = tf.layers.dense(prev_output, self.config.im_height * self.config.im_width * self.config.im_channels, kernel_initializer=tf.contrib.layers.xavier_initializer(),name='fc_out')
+        return tf.reshape(prev_output, (-1, self.config.im_height, self.config.im_width, self.config.im_channels))
+
+    def add_loss_op(self, final_layer):
+        return tf.reduce_mean((final_layer - self.truth_in) ** 2 / 2)
+
+    def add_training_op(self, loss):
+        learning_rate = tf.train.exponential_decay(self.config.lr, self.global_step, self.config.lr_decay_steps,
+                                                   self.config.lr_decay, staircase=True)
+        return tf.train.AdamOptimizer(learning_rate).minimize(loss, global_step=self.global_step)
+
+    def create_feed_dict(self, inputs_batch, outputs_batch=None):
+        feed_dict = {self.image_in: inputs_batch}
+        if outputs_batch is not None:
+            feed_dict[self.truth_in] = outputs_batch
+        return feed_dict
--- test.py
+++ test.py
@@ -1,21 +1,22 @@
 import tensorflow as tf
 import numpy as np
 from util import show_image_example
-from baseline_models import Conv2ConvModel
+from baseline_models import Conv2ConvModel, Affine2AffineModel
+import pickle
 
 params = {}
 params['lr'] = 1e-3
 params['lr_decay'] = .96
-params['lr_decay_steps'] = 100
-params['im_width'] = 128
-params['im_height'] = 128
+params['lr_decay_steps'] = 40
+params['im_width'] = 16
+params['im_height'] = 16
 params['im_channels'] = 3
-params['in_conv_layers'] = 0
+params['in_conv_layers'] = 1
 params['in_conv_filters'] = [1]
 params['in_conv_dim'] = [5]
 params['in_conv_stride'] = [2]
 params['fc_layers'] = 2
-params['fc_dim'] = [1024, 1024]
+params['fc_dim'] = [256, 256]
 params['embed_channels'] = 100
 params['out_conv_layers'] = 2
 params['out_conv_filters'] = [8, 16]
@@ -26,9 +27,9 @@ params['output_conv_stride'] = 1
 params["model_name"] = "test"
 params["ckpt_path"] = "ckpt"
 params["log_path"] = "log"
-params["n_epochs"] = 10
+params["n_epochs"] = 10000
 params["n_eval_batches"] = 10
-params["batch_size"] = 16
+params["batch_size"] = 4
 
 class TestGenerator:
     def __init__(self, w, h, c, b):
@@ -41,7 +42,7 @@ class TestGenerator:
         x = []
         y = []
         for _ in range(self.b):
-            x_sample = np.random.uniform(-1, 1, (self.h, self.w, self.c))
+            x_sample = np.zeros((self.h, self.w, self.c))
             y_sample = x_sample+1
             x.append(x_sample)
             y.append(y_sample)
@@ -49,17 +50,16 @@ class TestGenerator:
                 return x_sample, y_sample
         return x, y
 
-gen = TestGenerator(128, 128, 3, 1)
-train_tuples = [gen.next() for _ in range(1000)]
+
+train_tuples, valid_tuples, test_tuples = pickle.load(open("data/data (1).p", "rb"), encoding='latin1')
 train_examples = [np.array([np.array(tup[0]) for tup in train_tuples]), np.array([np.array(tup[1]) for tup in train_tuples])]
-valid_tuples = [gen.next() for _ in range(100)]
 valid_examples = [np.array([np.array(tup[0]) for tup in valid_tuples]), np.array([np.array(tup[1]) for tup in valid_tuples])]
 
 
-m = Conv2ConvModel(params)
+m = Affine2AffineModel(params)
 m.build()
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    m.fit(sess, tf.train.Saver(), train_examples, valid_examples)
+    m.fit(sess, None, train_examples, valid_examples)
     show_image_example(sess, m, train_tuples[0][0], train_tuples[0][1])
 

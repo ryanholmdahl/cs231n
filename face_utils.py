57b58bd4513149af4220c1fb7cce9f91dcdc8d3c
Ryan Holmdahl <ryholmdahl@gmail.com>
2017-05-18 22:13:07 -0700

    Face cropping!

---
+++ face_utils.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+
+class Cropper:
+    def __init__(self, cascade_path, flip_image=False):
+        self.classifier = cv2.CascadeClassifier(cascade_path)
+        self.flip_image = flip_image
+
+    def get_crop(self, image):
+        gray = image
+        if self.flip_image:
+            gray = np.fliplr(gray)
+        faces = self.classifier.detectMultiScale(gray, 1.1)
+        retval = None
+        max_w = 0
+        for (x,y,w,h) in faces:
+            w = max(w,h)
+            h = max(w,h)
+            if w > max_w:
+                retval = gray[y:y+h, x:x+w]
+                max_w = w
+        return retval

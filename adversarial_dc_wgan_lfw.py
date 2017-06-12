fix image scaling issue

adversarial_dc_wgan_lfw.py
@@ -16,7 +16,7 @@ from model_builder import ModularDiscriminator
 from utils.activation_funcs import leaky_relu
 from utils.util import minibatches, Progbar
 from lfw.dataset_builder import Dataset
-from scipy.misc import imsave
+from scipy.misc import imsave, toimage
 from tensorflow.examples.tutorials.mnist import input_data
 
 
@@ -32,9 +32,9 @@ class DC_WGAN():
         self.generator_epochs = 10000
         self.im_epochs = 1
         self.gaussian_epochs = 1
-        self.gen_lr = 1e-4
-        self.di_lr = 1e-3
-        self.dg_lr = 1e-3
+        self.gen_lr = 1e-5
+        self.di_lr = 1e-4
+        self.dg_lr = 1e-4
         self.lr_decay = 1
         self.lr_decay_steps = 100
         self.n_eval_batches = 10
@@ -371,14 +371,17 @@ class DC_WGAN():
         if not os.path.exists(path_name):
             os.makedirs(path_name)
         for i in range(len(outputs)):
-            imsave(os.path.join(path_name, "s{}_e{}.png".format(i, 0)), np.squeeze(outputs[i]))
-        imsave(os.path.join(path_name, "image_in.png"), np.squeeze(demo_image))
+            im = toimage(np.squeeze(outputs[i]), cmin=0, cmax=1)
+            im.save(os.path.join(path_name, "s{}_e{}.png".format(i, 0)))
+        im = toimage(np.squeeze(demo_image), cmin=0, cmax=1)
+        im.save(os.path.join(path_name, "image_in.png"))
         feed = {
             self.image_in: np.expand_dims(demo_image, 0),
             self.emotion_label: [demo_emotion]
         }
         decoded = self.pred_on_image_batch(feed, sess)
-        imsave(os.path.join(path_name, "image_out.png"), np.squeeze(decoded))
+        im = toimage(np.squeeze(decoded), cmin=0, cmax=1)
+        im.save(os.path.join(path_name, "image_out.png"))
 
     def restore_from_checkpoint(self, sess, saver, epoch):
         save_path = os.path.join(self.ckpt_path, self.model_name + "_" + str(epoch))
@@ -605,5 +608,5 @@ if __name__ == '__main__':
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         saver = tf.train.Saver()
-        #m.restore_from_checkpoint(sess, saver, 500)
+        m.restore_from_checkpoint(sess, saver, 2300)
         m.fit(sess, saver, d.train_examples, d.dev_examples)

compare_with_lfw.py
@@ -1,26 +1,29 @@
 from lfw.dataset_builder import Dataset
-from scipy.misc import imread, imsave
+from scipy.misc import imread, imsave, imresize, toimage
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-target_dir = "outputs\\100style_0image_5gauss_5train_gaussianlabels_decoder_5gauss_1image_5dropout_lfw_tanhnoscale_inconv_1000_1005"
-target_iter = "4065"
+target_dir = "outputs\\100style_0image_5gauss_5train_gaussianlabels_decoder_5gauss_1image_5dropout_lfw_tanhnoscale_inconv_1000_1005_slow"
+target_iter = "2300"
 
-d = Dataset((32, 32, 1))
+d = Dataset((32, 32, 1), split=[1, 0, 0])
 d.read_samples('lfw/lfw_data')
-train_images = np.concatenate((d.train_examples[0], d.dev_examples[0], d.test_examples[0]), axis=0)
+#train_images = np.concatenate((d.train_examples[0], d.dev_examples[0], d.test_examples[0]), axis=0)
+train_images = d.train_examples[0]
+print(train_images.shape)
 dir_path = os.path.join(target_dir, target_iter)
 for filename in os.listdir(dir_path):
     if "_closest" in filename:
         continue
-    im = imread(os.path.join(dir_path, filename))/255.0
+    im = imresize(imread(os.path.join(dir_path, filename), mode='L'), d.dims)/255.0
     min_loss = float('inf')
     best_im = None
-    for i in range(len(train_images)):
-        l2 = np.sum((im - train_images[i, :, :, 0])**2)
+    for i in range(train_images.shape[0]):
+        l2 = np.sum((im - np.squeeze(train_images[i, :, :, :]))**2)
         if l2 < min_loss:
             min_loss = l2
             best_im = train_images[i, :, :, 0]
     print(min_loss)
-    imsave(os.path.join(dir_path,(filename.split(".")[0])+"_closest.png"), best_im)
+    im = toimage(best_im, cmin=0, cmax=1)
+    im.save(os.path.join(dir_path,(filename.split(".")[0])+"_closest.png"))
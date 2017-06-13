a5d786953c974e8889cda5d94b8fba1cf512f15e
schopra8 <schopra8@stanford.edu>
2017-06-12 19:03:53 -0700

    Removing dead files

--- data/get_data.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-wget https://s3.amazonaws.com/nist-srd/SD18/sd18.zip
-unzip sd18.zip
-wget -v -c -r --no-parent --no-host-directories -e robots=off http://user21698:Og6Tf2Qn@nigos.nist.gov:8080/colorferet/
